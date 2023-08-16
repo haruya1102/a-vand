848735894e57bd39abff3332c2ee8b2ddfaf2c96
haruya1102 <haruyaiioni@gmail.com>
2023-08-16 23:37:53 +0900

    0.0.4

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -2,7 +2,6 @@
 
 import React from 'react'
 import { Link } from 'react-router-dom'
-import { auth } from './Firebase';
 
 // ヘッダー
 
@@ -16,19 +15,16 @@ const Header = () => {
                         <Link to="/" class=" text-white self-center text-xl lg:text-3xl font-semibold whitespace-nowrap">A-VAND</Link>
                     </h1>
                     <div class="flex items-center">
-                        <Link to="/" class="text-white hover:text-white/75 text-sm lg:text-xl inline-flex items-center font-semibold mr-2 lg:mr-16">
+                        <Link to="/" class="text-white hover:text-white/75 text-sm lg:text-xl inline-flex items-center font-semibold mr-4 lg:mr-16">
                             ホーム
                         </Link>
-                        <Link to="/user-list" class="text-white hover:text-white/75 text-sm lg:text-xl inline-flex items-center font-semibold mr-2 lg:mr-16" aria-controls="mobile-menu-2" aria-expanded="false">
+                        <Link to="/user-list" class="text-white hover:text-white/75 text-sm lg:text-xl inline-flex items-center font-semibold mr-4 lg:mr-16" aria-controls="mobile-menu-2" aria-expanded="false">
                             ユーザーリスト
                         </Link>
                         <Link to="/mypage" class="text-white hover:text-white/75 text-sm lg:text-xl inline-flex items-center font-semibold">
                             マイページ
                         </Link>
                     </div>
-                    <button onClick={() => auth.signOut()} class="text-white  bg-accent hover:bg-accent/90 inline-flex items-center font-semibold rounded-lg text-sm lg:text-lg px-2 lg:px-3 py-3 lg:py-4" aria-controls="mobile-menu-2" aria-expanded="false">
-                        <Link to="/sign-in" >サインアウト</Link>
-                    </button>
                 </div>
             </nav>
         </header>
